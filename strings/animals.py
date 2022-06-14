a3a920f9110e4d896a91afa9b63cfb73e1eb240c
EricRS <EricRS75@users.noreply.github.com>
2022-06-14 17:11:51 +0000

    copying local files with python

---
+++ strings/animals.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# create a small string of animal names
+ani_string = ["fish", "frog", "fly"]
+
+# print animal list
+print(ani_string)
+
+
+# change the list to capitalize
+ani_string2 =(ani_string[0].capitalize())
+ani_string2.append =(ani_string[1].capitalize())
+
+
+
+# display second list
+print(ani_string2)
+
